5993d1cef9beefb74f02627ab4d8669435c5652f
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2026-04-04 10:23:43 +0530

    Create complexMatrixAddition.cpp

---
+++ complexMatrixAddition.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class complex{
+    int r,i;
+    public:
+        complex(int rl = 0,int im = 0){
+            r = rl;
+            i = im;
+        }
+        complex operator + (const complex &obj){
+            return complex(r + obj.r , i + obj.i);
+        }
+    /*    complex operator - (const complex &obj){
+            return complex(r - obj.r , i - obj.i);
+        }
+      */
+      void display(){ cout << r << "  - " << i << "i" << endl; }
+      
+};
+class Matrix{
+    complex mat[2][2];
+    public:
+       Matrix(complex m[2][2]){ 
+           for(int i = 0;i<2;i++){
+               for(int j = 0;j<2;j++){
+                   mat[i][j] = m[i][j];
+               }
+           }
+       }
+       Matrix operator + ( const Matrix &m){
+           complex res[2][2];
+           for(int i = 0;i<2;i++){
+               for(int j = 0;j<2;j++){
+                 res[i][j] = mat[i][j] + m.mat[i][j];
+               }
+           }
+           return Matrix(res);
+       }
+        void display() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++)
+                mat[i][j].display();
+            cout << endl;
+        }
+    }
+};
+int main(){
+     complex a[2][2] = {
+        {complex(1,2), complex(3,4)},
+        {complex(2,1), complex(4,3)}
+    };
+
+    complex b[2][2] = {
+        {complex(5,6), complex(7,8)},
+        {complex(6,5), complex(8,7)}
+    };
+    Matrix m(a),n(b);
+    Matrix m3 = m + n;
+    m3.display();
+    return 0;
+}
