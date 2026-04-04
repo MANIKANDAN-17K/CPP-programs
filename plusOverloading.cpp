46985761da94dcdf28e91bfd465714d8696cda6c
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2026-04-04 09:27:25 +0530

    Create plusOverloading.cpp

---
+++ plusOverloading.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Number{
+    int value;
+    public:
+        Number(int val){ value = val;}
+        Number operator + (Number obj){
+            Number temp(0);
+            temp.value = value + obj.value;
+            return temp;
+        }
+        void display(){
+            cout << "value :" << value <<endl;
+        }
+};
+int main(){
+    Number n1(10),n2(20);
+    Number n3 = n1 + n2;
+    n3.display();
+    return 0;
+}
